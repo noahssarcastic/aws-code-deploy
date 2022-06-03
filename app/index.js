1a34733e9b02a493967dcb0f8e76d763905da5f3
Noah Iniguez <niniguezg@gmail.com>
2022-06-03 10:32:05 -0500

    Fix app to use relative file paths.

--- app/index.js
+++ app/index.js
@@ -1,9 +1,10 @@
 const http = require("http");
 const fs = require("fs");
+const path = require("path");
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "content-type": "text/html" });
-  fs.createReadStream("app/index.html").pipe(res);
+  fs.createReadStream(path.resolve(__dirname, "index.html")).pipe(res);
 });
 
 server.listen(process.env.PORT || 3000);
